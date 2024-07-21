97795c6464ba0cc41d825066447bf662eac22542
Ali Albustami <alibustami3@gmail.com>
2024-07-21 14:32:20 +0300

    add json loader function to load init stats

--- src/utils/utils_funcs.py
+++ src/utils/utils_funcs.py
@@ -1,9 +1,34 @@
-from typing import Tuple
+"""Utility functions for the project."""
+
+import json
+from typing import List, Tuple
+
+# def floats_converter(num: float) -> Tuple[int, int]:
+#     dec_len = len(str(num)) - len(str(int(num))) - 1
+
+#     int_num = int(str(num).replace(".", ""))
+
+#     return int_num, max(dec_len, 0)
 
 
-def floats_converter(num: float) -> Tuple[int, int]:
-    dec_len = len(str(num)) - len(str(int(num))) - 1
+def load_init_states(json_path: str) -> List[List[float]]:
+    """Load the initial states from the given JSON file.
 
-    int_num = int(str(num).replace(".", ""))
+    Parameters
+    ----------
+    json_path : str
+        Path to the JSON file containing the initial states.
 
-    return int_num, max(dec_len, 0)
+    Returns
+    -------
+    List[List[float]]
+        List of initial states.
+    """
+    with open(json_path, "r") as f:
+        init_states = json.load(f)
+
+    init_states_list = []
+    for value in init_states.values():
+        init_states_list.append(eval(value))
+
+    return init_states_list
